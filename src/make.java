fbaaf6a84aea3d1615237885f4cf4d1535d62426
hand <km@example.com>
2023-02-12 15:14:52 +0800

    My first version control

---
+++ src/make.java
@@ -0,0 +1,32 @@
+/**
+ * @author hand
+ * @date 2023/2/11 16:20\
+ * @name make
+ */
+
+
+public class make {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * @param i
+     * @return
+     */
+    public String compute(int i)
+    {
+        /*
+        *@Author:Hand
+        *@Data:2023/2/11 16:34
+        *@Param:[i]
+        *Return:java.lang.String
+        */
+        return "i";
+    }
+}
